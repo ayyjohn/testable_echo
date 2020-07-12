cc1ba2bf2f5c9f0683d7f4118c3c5bcf8d4bc0a9
Alec Johnson <alecjohnson55@gmail.com>
2020-07-12 10:38:41 -0700

    add newline test and capsys to all other tests

--- test_myecho.py
+++ test_myecho.py
@@ -1,22 +1,38 @@
+import random
+import string
 import pytest
+
 from myecho import myecho, parse_args
 
 
 def test__echo__with_no_args__returns_newline(capsys):
     parsed_args = parse_args([])
-    myecho(parsed_args)
-    captured = capsys.readouterr()
 
+    myecho(parsed_args)
+
+    captured = capsys.readouterr()
     assert captured.out == "\n"
 
 
-def test__echo__with_newline():
+def test__echo__with_n__strips_newline(capsys):
+    random_other_args = [
+        "".join(random.choice(string.ascii_lowercase))
+        for i in range(5)
+    ]
+    args = random_other_args + ["-n"]
+    parsed_args = parse_args(args)
+
+    myecho(parsed_args)
+
+    captured = capsys.readouterr()
+    assert not captured.out.endswith("\n")
+    for arg in random_other_args:
+        assert arg in captured.out
+
+
+def test__echo__with_escape_chars(capsys):
     assert False
 
 
-def test__echo__with_escape_chars():
-    assert False
-
-
-def test__echo__with_escape_chars_and_e_on():
+def test__echo__with_escape_chars_and_e_on(capsys):
     assert False
