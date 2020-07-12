81214b8bf38a060f7a16973169d1a078536f37ea
Alec Johnson <alecjohnson55@gmail.com>
2020-07-11 21:44:43 -0700

    wrap method in main

--- myecho.py
+++ myecho.py
@@ -12,40 +12,47 @@ ESCAPE_CHARS = {
     "\\\\": "\\",
 }
 
-parser = argparse.ArgumentParser(
-    prog="myecho",
-    description="my implementation of echo. writes any number of operands separated by single blank space characters and followed by a newline to stdout",
-)
-parser.add_argument(
-    "-n",
-    action="store_true",
-    help="do not print the trailing newline character",
-)
-parser.add_argument(
-    "-e",
-    action="store_true",
-    help="interpret backslash-escaped characters (\\a \\b \\c \\E \\f \\n \\r \\t \\v \\\\)",
-)
-parser.add_argument(
-    "-E",
-    action="store_true",
-    help="explicitly turn off the above backslash-escaped characters",
-)
-parser.add_argument(
-    "ARGS",
-    help="any number of other words",
-    nargs="*"
-)
 
-args = parser.parse_args()
-print_end = "" if args.n else "\n"
+def myecho():
 
-output = ""
-for arg in args.ARGS:
-    if arg in ESCAPE_CHARS:
-        output += ESCAPE_CHARS[arg] if args.e else arg
-    else:
-        output += arg
-    output += " "
+    parser = argparse.ArgumentParser(
+        prog="myecho",
+        description="my implementation of echo. writes any number of operands separated by single blank space characters and followed by a newline to stdout",
+    )
+    parser.add_argument(
+        "-n",
+        action="store_true",
+        help="do not print the trailing newline character",
+    )
+    parser.add_argument(
+        "-e",
+        action="store_true",
+        help="interpret backslash-escaped characters (\\a \\b \\c \\E \\f \\n \\r \\t \\v \\\\)",
+    )
+    parser.add_argument(
+        "-E",
+        action="store_true",
+        help="explicitly turn off the above backslash-escaped characters",
+    )
+    parser.add_argument(
+        "ARGS",
+        help="any number of other words",
+        nargs="*"
+    )
 
-print(output, end=print_end)
+    args = parser.parse_args()
+    print_end = "" if args.n else "\n"
+
+    output = ""
+    for arg in args.ARGS:
+        if arg in ESCAPE_CHARS:
+            output += ESCAPE_CHARS[arg] if args.e else arg
+        else:
+            output += arg
+        output += " "
+
+    print(output, end=print_end)
+
+
+if __name__ == "__main__":
+    myecho()
