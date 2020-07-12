a067fe7fac9e2fc3bb29d59aed1b131096d8c499
Alec Johnson <alecjohnson55@gmail.com>
2020-07-11 21:51:07 -0700

    add test cases empty

---
+++ test_myecho.py
@@ -0,0 +1,18 @@
+import pytest
+from myecho import myecho
+
+
+def test__empty__echo():
+    pass
+
+
+def test__echo__with_newline():
+    pass
+
+
+def test__echo__with_escape_chars():
+    pass
+
+
+def test__echo__with_escape_chars_and_e_on():
+    pass
